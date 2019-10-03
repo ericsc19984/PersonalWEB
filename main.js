bdb9e669ac482e0f7073c4a996fa12fd85a8e82b
Eric San Clemente <2013.3a.esanclemente@gmail.com>
2019-10-03 11:42:37 +0200

    Js

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
-let palabraInicial = "hola mundo"
-
-for(item in palabraInicial){
-    console.log(item)
-}
+const showMenu = (e) => {
+    let menu = document.getElementById('disp-none');
+    menu.style.display = "block";
+    menu.style.zIndex = "0";
+    eventMenu.removeEventListener('click', showMenu);
+    eventMenu.addEventListener('click', hideMenu);
+  }
+  const hideMenu = (e) => {
+    let menu = document.getElementById('disp-none');
+    menu.style.display = "none";
+    menu.style.zIndex = "-100";
+    eventMenu.removeEventListener('click', hideMenu);
+    eventMenu.addEventListener('click', showMenu);
+  }
+  let eventMenu = document.getElementById('switcher');
+  eventMenu.addEventListener('click', showMenu);
